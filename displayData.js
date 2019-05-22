6acea7f85ee0b842a4a0c37a944eeede3ae74468
ClintonMedbery <clintomed@gmail.com>
2019-05-22 09:34:52 -0400

    Part 2

--- displayData.js
+++ displayData.js
@@ -3,16 +3,50 @@ const profitAndLoss = {
         january: "1000.00",
         february: "2000.00",
         march: "3000.00"
+    },
+    cogs: {
+        january: "2000.00",
+        february: "4000.00",
+        march: "5000.00"
+    },
+    facility: {
+        january: "300.00",
+        february: "400.00",
+        march: "500.00"
     }
 }
 
-loadTable = () => {
-    var revenueData = profitAndLoss.revenue;
+loadTable = (financialSelection) => {
+    let financialData;
+    let header;
+    if(financialSelection){
+        financialData = profitAndLoss[financialSelection];
+        header = capitalize(financialSelection);
+    } else {
+        financialData = profitAndLoss.revenue;
+        header = "Revenue";
+    }
     let data = "";
-    Object.keys(revenueData).forEach(function (key){
-        data += `<p>${capitalize(key)}: ${revenueData[key]}</p>`;
+    Object.keys(financialData).forEach(function (key){
+        data += `<p>${capitalize(key)}: ${financialData[key]}</p>`;
     });
+    let financialHeader = `<h4>${header}</h4>`;
+    document.getElementById("financialHeader").innerHTML = financialHeader;
+
     document.getElementById("data").innerHTML = data;
+}
+
+generateDropdown = () => {
+    var revenueData = profitAndLoss;
+    let data = `<select id="financesSelect" name="financesSelect" onchange="updateFinancials(this)">`;
+    Object.keys(revenueData).forEach(function (key){
+        data += `<option value="${key}">${capitalize(key)}</option>`;
+    });
+    document.getElementById("dropdown").innerHTML = data;
+}
+
+updateFinancials = (selection) => {
+    loadTable(selection.value);
 }
 
 const capitalize = (stringToCapitalize) => {
